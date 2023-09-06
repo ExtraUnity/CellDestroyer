e62172c42b59ea2760c0e4bfac73183e2f53a97f
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2023-09-06 15:25:03 +0200

    Added a binary threshold

--- src/main.c
+++ src/main.c
@@ -1,55 +1,58 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "cbmp.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
 unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGHT];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
 
-void formatOutputImage(unsigned char input[BMP_WIDTH][BMP_HEIGHT])
-{
-    for (int i = 0; i < BMP_WIDTH; i++)
-    {
-        for (int j = 0; j < BMP_HEIGHT; j++)
-        {
-            for (int k = 0; k < BMP_CHANNELS; k++)
-            {
-                output_image[i][j][k] = input[i][j];
-            }
-        }
-    }
+void binaryThreshold(int i, int j, int value) {
+  if (value >= 90) {
+    greyscale_image[i][j] = 255;
+  }
+  if (value < 90) {
+    greyscale_image[i][j] = 0;
+  }
 }
 
-int main(int argc, char **argv)
-{
-
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-        exit(1);
+void formatOutputImage(unsigned char input[BMP_WIDTH][BMP_HEIGHT]) {
+  for (int i = 0; i < BMP_WIDTH; i++) {
+    for (int j = 0; j < BMP_HEIGHT; j++) {
+      for (int k = 0; k < BMP_CHANNELS; k++) {
+        binaryThreshold(i,j, input[i][j]);
+        output_image[i][j][k] = input[i][j];
+      }
     }
+  }
+}
 
-    // Load image from file
-    read_bitmap(argv[1], input_image);
+int main(int argc, char **argv) {
 
-    
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n",
+            argv[0]);
+    exit(1);
+  }
 
-    for (int i = 0; i < BMP_WIDTH; i++)
-    {
-        for (int j = 0; j < BMP_HEIGHT; j++)
-        {
-            greyscale_image[i][j] = (input_image[i][j][0] + input_image[i][j][1] + input_image[i][j][2]) / 3;
-        }
+  // Load image from file
+  read_bitmap(argv[1], input_image);
+
+  for (int i = 0; i < BMP_WIDTH; i++) {
+    for (int j = 0; j < BMP_HEIGHT; j++) {
+      greyscale_image[i][j] =
+          (input_image[i][j][0] + input_image[i][j][1] + input_image[i][j][2]) /
+          3;
     }
-    /*
-    Find all cells
-    */
+  }
+  /*
+  Find all cells
+  */
 
-    formatOutputImage(greyscale_image);
+  formatOutputImage(greyscale_image);
 
-    // Save image to file
-    write_bitmap(output_image, argv[2]);
-    printf("Number of cells has not been counted yet :(");
-    return 0;
+  // Save image to file
+  write_bitmap(output_image, argv[2]);
+  printf("Number of cells has not been counted yet :(");
+  return 0;
 }
